368fa70460754c2e6751efc3b658127a0a652289
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-04 23:39:12 +0100

    add random

--- casting.py
+++ casting.py
@@ -1,10 +1,14 @@
 x=str(3)
 y=int(3)
 z=float(3)
-#======== print value =========
+#======== print value ======================
 print(x)
 print(y)
 print(z)
 
-#========print the variable type ===========
+#========print the variable type ============
 print( type(z))
+
+#=========random ============================
+import random
+print(random.randrange(1,10))
--- turtle.py
+++ turtle.py
@@ -1,13 +1,13 @@
 import turtle
 colors=["red","violet","yellow","purple","orange","blue"]
 t=turtle.Pen()
-t.speed(1000)
+turtle.speed(1000)
 turtle.bgcolor("black")
 
 for i in range(200):
-    t.pencolor(color[i%6])
-    t.width(i/100 + 1)
-    t.forward(i)
-    t.left(59)
+    turtle.pencolor(colors[i%6])
+    turtle.width(i/100 + 1)
+    turtle.forward(i)
+    turtle.left(59)
 
 turtle.done()
