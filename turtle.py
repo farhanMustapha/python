987329b7b2225c452d56311a639f00d142f682a3
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-04 21:04:34 +0100

    add turtle ex

---
+++ turtle.py
@@ -0,0 +1,16 @@
+#turtel
+
+import turtle
+
+colors=["red","violet","yellow","purple","orange","blue"]
+t=turtle.pen()
+t.speed(1000)
+turtle.bgcolor("black")
+
+for i in range(200):
+    t.pencolor(color[i%6])
+    t.width(i/100 + 1)
+    t.forward(i)
+    t.left(59)
+
+turtle.done()
