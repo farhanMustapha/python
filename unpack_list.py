fa4be7deebe983af4118e9a2e1e1070ef581e2eb
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-04 22:22:13 +0100

    add global key

--- unpack_list.py
+++ unpack_list.py
@@ -1,3 +1,10 @@
+#Déballer une collection
+#Si vous avez une collection de valeurs dans une liste, un tuple,
+#etc. Python vous permet d’extraire les valeurs en variables. 
+#C’est ce qu’on appellele déballage.
+
+
+
 fruit=["orange","lemon","avocat"]
 x,y,z=fruit
 
