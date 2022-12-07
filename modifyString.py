111f10e05e1b13c902f7a38ecf77b8f48cc941ef
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-07 20:50:28 +0100

    format string

--- modifyString.py
+++ modifyString.py
@@ -1,2 +1,7 @@
-a=" HELLO FROM THE OTHER SIDE"
-print(a.lower())
+#a=" HELLO FROM THE OTHER SIDE"
+#print(a.lower())
+qt=3 # {0}
+item=567 # {1}
+price=49.95 # {2}
+myorder="i want to pay {2} dirhams for {0} pieces ot item {1}"
+print(myorder.format(qt,item,price))
