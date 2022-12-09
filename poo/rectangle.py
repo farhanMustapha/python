c0504ad4cf33a1b53d77b39acef0e678e61101c8
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-09 23:58:45 +0100

    poo class rect

---
+++ poo/rectangle.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self,longueur,largeur):
+        self.longueur=longueur
+        self.largeur=largeur
+
+monRec=Rectangle(7,5)
+def surface():
+    s=monRec.largeur*monRec.longueur
+    print(s)
+
+
+surface()
