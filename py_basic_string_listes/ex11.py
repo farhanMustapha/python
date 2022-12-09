158a41a87b497000222d9096eac2a62ce6af388d
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-09 20:14:17 +0100

    delete the redandance

--- py_basic_string_listes/ex11.py
+++ py_basic_string_listes/ex11.py
@@ -1,5 +1,8 @@
 s="python.org"
+l=[]
 for i in range(0,len(s)):
-    print(i,"- number of ", s[i]," in python is :",s.count(s[i]))
+    if s[i] not in l:
+        l.append(s[i])
+        print("- number of ", s[i]," in python is :",s.count(s[i]))
 
         
