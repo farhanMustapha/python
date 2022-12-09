19a87dffa47bb1bf809eb56862c26b46f4eb170d
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-09 18:24:27 +0100

    table de multiplication

---
+++ py_basic_string_listes/ex09.py
@@ -0,0 +1,14 @@
+#table de multiplication de n
+"""
+n=int(input("tapez un numero : "))
+for i in range(0,10):
+    print("multiplication de ",n,"*",i,"egale à :"+str(i*n))
+
+"""
+
+
+for i in range(0,10):
+    print("===========table de "+str(i)+"======================")
+    for j in range(0,10):
+         print("multiplication de ",i,"*",j,"egale à :"+str(i*j))
+        
