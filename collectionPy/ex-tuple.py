9d52cb683c8871ccb5e97eecbd5a248652cc4234
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-11 21:23:08 +0100

    slicing tuple 2

--- collectionPy/ex-tuple.py
+++ collectionPy/ex-tuple.py
@@ -3,6 +3,7 @@
 #================= exercice 1 : Reverse the tuple =======================
 journal=("achat","vent","banque","caisse","operation divers")
 tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+t=("hello world")
 """t={}
 for i in range(0,len(journal)):
     t[i]=journal[len(journal)-i-1]
@@ -10,4 +11,4 @@ for i in range(0,len(journal)):
 print(t)"""
 
 #print(journal[::-1])
-print(tuplex[::4])
+print(t[0:11:1])
