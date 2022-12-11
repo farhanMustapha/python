d868a79ee584c22ba7bcc720561fbfaf3f8aa7e4
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-11 21:50:57 +0100

    unpack tuple

--- collectionPy/ex-tuple.py
+++ collectionPy/ex-tuple.py
@@ -26,5 +26,10 @@ print(listo[1])
 """
 
 #====================exercice 3 : creat tuple with single item =======================
-tupleo=(50)
-print(tupleo)
+#tupleo=(50)
+#print(tupleo)
+
+#====================exercice 4 :Unpack the tuple into 4 variables ==================
+tuple1 = (10, 20, 30, 40)
+a,b,c,d=tuple1
+print(a,b,c,d)
