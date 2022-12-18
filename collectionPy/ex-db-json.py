360ceb017109093d537726d3a072587bf1272dd2
farhanMustapha <38401627+farhanMustapha@users.noreply.github.com>
2022-12-18 12:57:10 +0100

    extention file programme

--- collectionPy/ex-db-json.py
+++ collectionPy/ex-db-json.py
@@ -1,4 +1,5 @@
 import json
+
 data={
     "key1":"value1",
     "key2":"value2",
---
+++ lesFichiers/fichierExtention.py
@@ -0,0 +1,16 @@
+
+#===programme permet de donner l'extention du fichier =======
+
+fichier=input("saisire le nom du fichier :")
+l=fichier.split(" ")
+print(l)
+if(l[1]=="pdf"):
+    print("the extention is ",l[1])
+if(l[1]=="xlsx"):
+    print("the extention is ",l[1])
+if(l[1]=="csv"):
+    print("the extention is ",l[1])
+if(l[1]=="txt"):
+    print("the extention is ",l[1])
+else:
+    print("your file has no extention")
